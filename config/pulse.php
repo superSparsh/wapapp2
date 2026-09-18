d31948ab696a4381b3bae1df9c441b72e1766cd5
Sparsh Thakur <thakursparsh.st@gmail.com>
2026-09-18 13:52:02 +0530

    update

--- config/pulse.php
+++ config/pulse.php
@@ -141,7 +141,7 @@ return [
             'enabled' => env('PULSE_CACHE_INTERACTIONS_ENABLED', true),
             'sample_rate' => env('PULSE_CACHE_INTERACTIONS_SAMPLE_RATE', 1),
             'ignore' => [
-                ...Pulse::defaultVendorCacheKeys(),
+                ...(class_exists(Pulse::class) ? Pulse::defaultVendorCacheKeys() : []),
             ],
             'groups' => [
                 '/^job-exceptions:.*/' => 'job-exceptions:*',
