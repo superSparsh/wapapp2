cda0078cea4c24a06cb48c3da8c5195070854104
Sparsh Thakur <thakursparsh.st@gmail.com>
2026-09-23 11:54:13 +0530

    update

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\HorizonServiceProvider;
 use App\Providers\MacroServiceProvider;
 use App\Providers\TenancyServiceProvider;
 
 return [
     AppServiceProvider::class,
+    HorizonServiceProvider::class,
     MacroServiceProvider::class,
     TenancyServiceProvider::class,
 ];
